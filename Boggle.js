0f8549981916dc27bb79722569dcfe3b0197a1ad
Rodrigote <rodrigojali@gmail.com>
2021-03-21 20:04:09 -0300

    chore: minor changes in comments

--- Boggle.js
+++ Boggle.js
@@ -1,10 +1,10 @@
 const boggleFinder = (matrix, word) => {
   let wordExists = false;
 
-  //iterate over the given matrix
+  // Iterate over the given matrix
   for (let row = 0; row < matrix.length; row += 1) {
     for (let col = 0; col < matrix[0].length; col += 1) {
-      //do DFS search for the word, on first character in the word
+      // Do DFS search for the word, on first character in the word
       if (characterFinder(matrix, word, row, col, 0)) wordExists = true;
     }
   }
@@ -17,31 +17,31 @@ const characterFinder = (matrix, word, row, col, currPos) => {
   const columns = matrix[0].length;
   const nextChar = currPos + 1;
 
-  //out of boundaries early returns
+  // Out of boundaries early returns
   if (row < 0 || row >= rows
    || col < 0 || col >= columns
   ) return false;
 
-  //if lookup character found in current position
+  // If lookup character found in current position
   if (matrix[row][col] === word.charAt(currPos)) {
-    //copy the found char, flag it to avoid re-using it and let
-    //let the DFS do the search
+    // Copy the found char, flag it to avoid re-using it and let
+    // let the DFS do the search
     const cachedChar = matrix[row][col];
 
     matrix[row][col] = 0;
-  	//check if it's the last character in the word
+  	// Check if it's the last character in the word
     if (currPos === word.length - 1) return true
     
-    //else, check with DFS the four adjacent tiles
-    //for the next character in the word
+    // Else, check with DFS the four adjacent tiles
+    // for the next character in the word
     if (
-      characterFinder(matrix, word, row - 1, col, nextChar) || //up
-      characterFinder(matrix, word, row, col + 1, nextChar) || //right
-      characterFinder(matrix, word, row + 1, col, nextChar) || //down
-      characterFinder(matrix, word, row, col - 1, nextChar)    //left
+      characterFinder(matrix, word, row - 1, col, nextChar) || // Up
+      characterFinder(matrix, word, row, col + 1, nextChar) || // Right
+      characterFinder(matrix, word, row + 1, col, nextChar) || // Down
+      characterFinder(matrix, word, row, col - 1, nextChar)    // Left
     ) return true;
 
-    //reset the currChar value
+    // Reset the currChar value
     matrix[row][col] = cachedChar
   }
 
