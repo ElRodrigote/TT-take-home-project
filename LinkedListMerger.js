8b276aead859c79134e5e0652768fd30665ddbff
Rodrigote <rodrigojali@gmail.com>
2021-03-21 21:33:55 -0300

    refactor: minor change in namespaces and comments

--- LinkedListMerger.js
+++ LinkedListMerger.js
@@ -8,7 +8,7 @@
 
 /* This solution applies divide & conquer with mergePair function,
  * merging lists pairs until getting a final result.
- * This solution is O(logK), applying recursion.
+ * This solution is O(logK(N)), applying recursion.
  */
 
 const mergeLists = (listsArr) => {
@@ -21,18 +21,18 @@ const mergeLists = (listsArr) => {
   return sentinel;
 };
 
-const mergePair = (listA, listB) => {
-  // Check if one of the given Lists doesn't exists, and return the other one
-  if (!listA || !listB) return listA || listB;
+const mergePair = (nodeA, nodeB) => {
+  // Check if one of the given Nodes doesn't exists, and return the other one
+  if (!nodeA || !nodeB) return nodeA || nodeB;
 
   // Compare the two current values, and link them in ascendent order
-  if (listA.val < listB.val) {
-      listA.next = mergePair(listA.next, listB);
+  if (nodeA.val < nodeB.val) {
+      nodeA.next = mergePair(nodeA.next, nodeB);
 
-      return listA;
+      return nodeA;
   } 
 
-  listB.next = mergePair(listA, listB.next);
+  nodeB.next = mergePair(nodeA, nodeB.next);
 
-  return listB; 
+  return nodeB; 
 };
